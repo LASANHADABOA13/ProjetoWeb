456b4661fec48e92181e73aadf245bd4e3edc4d5
Alt_Mz <86251053+LASANHADABOA13@users.noreply.github.com>
2021-12-23 22:26:23 -0300

    Projeto web
    sla

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+const menu_mobile = document.getElementById('open_m');
+
+function showMenu(){
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+}
+
+menu_mobile.addEventListener('click', showMenu);
